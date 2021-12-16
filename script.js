9e584776da7e325f5a980fcd35cea6a0c33d99ca
Michael <michaelmouet@gmail.com>
2021-12-16 14:46:54 -0800

    1st Commit: Added index.html, script.js, and styles.css files.

---
+++ script.js
@@ -0,0 +1,28 @@
+// Michael Mouet - JS Number Counter JavaScript
+
+// INSTRUCTIONS
+// Build a simple counter.
+// The count starts at 0
+// Have two buttons on the screen, one to increase (+) the count by 1, and one button to lower (-) the count by 1.
+// Pressing the plus button increases the count by 1, pressing the minus button decreases by 1
+
+// PLANNING: 	
+// Write two functions, one to add and one to subtract 
+// Attach each function to a getElementById() to change the HTML value shown
+// add inner html
+// Write out variable representing the counter outside of function
+
+
+let counter = 0
+
+const addOne = () => {
+	counter ++
+	document.getElementById("p1").innerHTML = counter
+
+};
+
+const subtractOne = () => {
+	counter --
+	document.getElementById("p1").innerHTML = counter
+};
+
